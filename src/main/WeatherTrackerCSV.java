f8cd822dd55ec66ebaaf4bd107a0e7266f6502c9
brett.cunningham <brett.cunningham@sbevision.com>
2024-12-09 15:17:36 -0500

    wip(exercise): quick way to read in csv instead of relying on user input from scanner or terminal

--- src/main/WeatherTrackerCSV.java
+++ src/main/WeatherTrackerCSV.java
@@ -26,13 +26,58 @@ public class WeatherTrackerCSV {
 
         System.out.println("Data: " + Arrays.toString(data));
 
-//        weatherData.add(map())
+        mapToDailyWeatherData(data, weatherData);
+      }
+
+      for (int i=1; i<weatherData.size(); i++){
+        // Create an object to store the data
+        DailyWeatherData singleDayWeatherData = weatherData.get(i);
+
+        System.out.println("Enter weather data for day #" + i);
+        System.out.println("------------------------------");
+
+        System.out.print("High temperature: ");
+        System.out.println(singleDayWeatherData.highTemp);
+
+        System.out.print("Low temperature: ");
+        System.out.println(singleDayWeatherData.lowTemp);
+
+        System.out.print("Rainfall in inches: ");
+        System.out.println(singleDayWeatherData.rainfall);
+
+        System.out.print("Humidity from 0.0 - 1.0: ");
+        System.out.println(singleDayWeatherData.humidity);
+
+        System.out.print("Barometric pressure: ");
+        System.out.println(singleDayWeatherData.pressure);
+
+        System.out.print("Wind Speed: ");
+        System.out.println(singleDayWeatherData.windSpeed);
+
+        System.out.print("Wind Direction in degrees: ");
+        System.out.println(singleDayWeatherData.windDirection);
+
       }
 
     } catch (FileNotFoundException e) {
       e.printStackTrace();
     } catch (IOException e) {
       e.printStackTrace();
+    }
+  }
+
+  static void mapToDailyWeatherData(String[] data, ArrayList<DailyWeatherData> list) {
+    if(data.length > 0 && !data[0].equals("highTemp")){
+      int highTemp = Integer.parseInt(data[0]);
+      int lowTemp = Integer.parseInt(data[1]);
+      double rainfall = Double.parseDouble(data[2]);
+      double humidity = Double.parseDouble(data[3]);
+      double pressure = Double.parseDouble(data[4]);
+      int windSpeed = Integer.parseInt(data[5]);
+      int windDirection = Integer.parseInt(data[6]);
+
+      list.add(new DailyWeatherData(highTemp, lowTemp, rainfall, humidity, pressure, windSpeed, windDirection));
+
     }
 
 
